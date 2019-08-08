da0ef940d8ddde31469c9862762e78a36ee7152c
Athen Osterberg <osterbergathen@gmail.com>
2019-08-07 22:10:18 -0500

    Add files via upload

--- AutomatedQAHW.py
+++ AutomatedQAHW.py
@@ -1,6 +1,30 @@
 from selenium import webdriver
+import time
 
+#basic functionality
 driver = webdriver.Chrome()
 driver.get("https://evereve.com/")
 
+#I found that the program executes too fast so I added a wait time
+time.sleep(5)
+
+section = driver.find_element_by_id("mini_banner_buttonA")
+section.click()
+
+time.sleep(5)
+
+item = driver.find_element_by_class_name("product-item-link")
+item.click()
+
+color = driver.find_element_by_id("option-label-color-166-item-8")
+color.click()
+
+size = driver.find_element_by_id("option-label-size-167-item-26")
+size.click()
+
+add_to_bag = driver.find_element_by_id("product-addtocart-button")
+add_to_bag.click()
+
+go_to_bag = driver.find_element_by_id("cart-black")
+go_to_bag.click()
 # driver.quit()
