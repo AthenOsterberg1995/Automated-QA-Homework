a44ae4a56d3016dea6846b8313c6525f95486ab1
Athen Osterberg <osterbergathen@gmail.com>
2019-08-07 22:55:09 -0500

    Add files via upload

--- AutomatedQAHW.py
+++ AutomatedQAHW.py
@@ -1,30 +1,62 @@
+#selenium is the autometed testing framework
 from selenium import webdriver
-import time
 
-#basic functionality
-driver = webdriver.Chrome()
-driver.get("https://evereve.com/")
+#Test opens the home page
+try:
+    driver = webdriver.Chrome()
+    driver.get("https://evereve.com/")
+except:
+    print("webdriver is missing")
 
-#I found that the program executes too fast so I added a wait time
-time.sleep(5)
+#Test looks for a section to shop through    
+try:
+    section = driver.find_element_by_id("mini_banner_buttonA")
+    section.click()
+except:
+    print("Element for shopping section cannot be found.")
 
-section = driver.find_element_by_id("mini_banner_buttonA")
-section.click()
+#Test looks for a product to click on
+try:
+    item = driver.find_element_by_class_name("product-item-link")
+    item.click()
+except:
+    print("Element for item cannot be found.")
 
-time.sleep(5)
+#Test selects a color
+try:
+    color = driver.find_element_by_id("option-label-color-166-item-8")
+    color.click()
+except:
+    print("Element for color cannot be found.")
 
-item = driver.find_element_by_class_name("product-item-link")
-item.click()
+#test selects a size
+try:
+    size = driver.find_element_by_id("option-label-size-167-item-26")
+    size.click()
+except:
+    print("Element for size cannot be found.")
 
-color = driver.find_element_by_id("option-label-color-166-item-8")
-color.click()
+#test adds the item to the bag
+try:
+    add_to_bag = driver.find_element_by_id("product-addtocart-button")
+    add_to_bag.click()
+except:
+    print("Element for adding item to bag cannot be found or not all fields are selected.")
 
-size = driver.find_element_by_id("option-label-size-167-item-26")
-size.click()
+#test goes to the bag
+try:
+    go_to_bag = driver.find_element_by_id("cart-black")
+    go_to_bag.click()
+except:
+    print("Element for going to the bag cannot be found")
 
-add_to_bag = driver.find_element_by_id("product-addtocart-button")
-add_to_bag.click()
-
-go_to_bag = driver.find_element_by_id("cart-black")
-go_to_bag.click()
-# driver.quit()
+#I checked to see if the test would fail by commenting out add_to_bag.click()
+#to make sure it would fail with an empty bag.
+#Test checks for something in the bag
+try:
+    check_full = driver.find_element_by_class_name("action.primary.checkout")
+    print("Item added.")
+except:
+    print("Item not added")
+    
+driver.quit()
